b03cd62717028ee6a9284fc4e20ea06a57b301c9
MforMko <135072155+MforMko@users.noreply.github.com>
2023-12-23 20:22:11 +0000

    Added if statement to check height

--- js/scripts.js
+++ js/scripts.js
@@ -8,7 +8,13 @@ let pokemonList = [
 for (let i = 0; i < pokemonList.length; i++) {
     // Get the current Pokemon object
     let currentPokemon = pokemonList[i];
+    //conditional to check height & make a remark
+    if (pokemonList[i].height > 0.6){
+        document.write(`<p><b>Name:</b> ${currentPokemon.name}  <b>Height:</b> ${currentPokemon.height}m (Wow, that's big!)</p><br>`);
+        
+    } else if 
 
     // Print the name and height of the current Pokemon
-    document.write(`<p><b>Name:</b> ${currentPokemon.name},  <b>Height:</b> ${currentPokemon.height}m </p><br>`);
-}
+    (document.write(`<p><b>Name:</b> ${currentPokemon.name}    <b>Height:</b> ${currentPokemon.height}m </p><br>`));
+    }
+
