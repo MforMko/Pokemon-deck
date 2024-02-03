e311d73fc253d347d68c998e08b1dcf351088f05
MforMko <135072155+MforMko@users.noreply.github.com>
2024-02-03 15:49:59 +0000

    Added a modal

--- js/scripts.js
+++ js/scripts.js
@@ -20,9 +20,74 @@ let pokemonRepository = (function(){
       showDetails(pokemon);
     });
   }
+  function showModal(pokemon) {
+    let modalContainer = document.querySelector('#modal-container');
+  
+    // Clear all existing modal content
+    modalContainer.innerHTML = '';
+  
+    let modal = document.createElement('div');
+    modal.classList.add('modal');
+  
+    // Add the new modal content
+    let closeButtonElement = document.createElement('button');
+    closeButtonElement.classList.add('modal-close');
+    closeButtonElement.innerText = 'Close';
+    closeButtonElement.addEventListener('click', hideModal);
+
+    let nameElement = document.createElement('h1');
+    nameElement.innerText = pokemon.name;
+  
+    let heightElement = document.createElement('p');
+    heightElement.innerText = 'Height: ' + pokemon.height;
+
+    let weightElement = document.createElement('p');
+    weightElement.innerText = 'Weight: ' + pokemon.weight;
+    
+    let imageElement = document.createElement('img');
+    imageElement.src = pokemon.imageUrl;
+
+    let typesElement = document.createElement('p');
+    typesElement.innerText = 'Type: ' + pokemon.types.join(", ") + "</p>";
+    
+    modal.appendChild(closeButtonElement);
+    modal.appendChild(nameElement);
+    modal.appendChild(heightElement);
+    modal.appendChild(imageElement);
+    modal.appendChild(typesElement);
+    modal.appendChild(weightElement);
+    modalContainer.appendChild(modal);
+    modalContainer.classList.add('is-visible');
+
+    modalContainer.addEventListener('click', (e) => {
+      // Since this is also triggered when clicking INSIDE the modal
+      // We only want to close if the user clicks directly on the overlay
+      let target = e.target;
+      if (target === modalContainer) {
+        hideModal();
+      }
+    });
+  }
+  
+
+  function hideModal() {
+    let modalContainer = document.querySelector('#modal-container');
+    modalContainer.classList.remove('is-visible');
+
+    window.addEventListener('keydown', (e) => {
+      let modalContainer = document.querySelector('#modal-container');
+      if (e.key === 'Escape' && modalContainer.classList.contains('is-visible')) {
+        hideModal();  
+      }
+    });
+  }
+  //This is the method I need to edit to show a modal 
+  //with the Pokémon’s name, its height, and an image 
+  //of the Pokémon
   function showDetails (pokemon) {
     loadDetails(pokemon).then(function () {
-    console.log(pokemon);
+      /*console.log(pokemon);*/
+      showModal(pokemon);
     });
   }
   function loadList() {
@@ -48,6 +113,7 @@ let pokemonRepository = (function(){
       //Now we add the details to the item
       item.imageUrl = details.sprites.front_default;
       item.height = details.height;
+      item.weight = details.weight;
       item.types = details.types;
     }).catch(function (e) {
       console.error(e);
